a4b016513f12c340339e8962676fda088123c2ef
O'Brien Abucheri <abucheriobrien@gmail.com>
2023-08-25 22:29:05 +0300

    for mem management

---
+++ Memory.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *Memory_Void_FreePPU8 - Free allocated memory
+ *@Copy_PPU_P2P: pointer to pointer
+ *@Copy_U32_Size: size of the pointer
+ */
+void Memory_Void_FreePPU8(char **Copy_PPU_P2P, int Copy_U32_Size)
+{
+	int Local_U32_Counter = 0;
+
+	if (Copy_PPU_P2P == NULL)
+		return;
+	for (Local_U32_Counter = 0; Local_U32_Counter < Copy_U32_Size; Local_U32_Counter++)
+		free(Copy_PPU_P2P[Local_U32_Counter]);
+}
+
+/**
+ * Memory_Void_FreePU8 - Free allocated memory
+ * @Copy_PU8_Array: Input array
+ */
+void Memory_Void_FreePU8(char *Copy_PU8_Array)
+{
+	if (isatty(STDIN_FILENO))
+		_putchar('\n');
+	free(Copy_PU8_Array);	
+}
