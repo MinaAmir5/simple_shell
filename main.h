3f2229eae3f2d3f44f966e2a4f5e40c7a071b68d
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-08-26 03:32:15 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,30 +2,31 @@
 #define _MAIN_H_
 
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
 #include <limits.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 extern char **environ;
 
 int String_PU8_StrCmp(char *str1, char *str2);
+int Error_U32_Print(char **arguments, int counter);
+void Memory_Void_FreePPU8(char **grid, int heigth);
 int Path_U32_GetPath(char **arguments);
 int Execution_U32_ChechBuiltin(char **arguments, int exit_stat);
+int Execution_U32_Execute(char **arguments);
 char *Path_PU8_GetEnv(char *global_var);
+int Path_U32_CheckPath(char *pathname);
 char *String_PU8_StrDup(char *str);
 char *String_PU8_StrConcat(char *dir_path, char *command);
 void Memory_Void_FreePU8(char *entry);
+int String_U32_PutChar(char c);
 int String_U32_StrSplit(char *entry, char **arguments);
 int Error_U32_Status(int n);
-int Error_U32_Print(char **arguments, int counter);
-int String_U32_PutChar(char c);
-int Path_U32_CheckPath(char *pathname);
-int Execution_U32_Execute(char **arguments);
-void Memory_Void_FreePPU8(char **grid, int heigth);
 
 #endif
